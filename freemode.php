848dd73cc0a3595f97adf9dda32ca790e6734331
VoMenThan <thancn12@gmail.com>
2016-12-21 15:31:07 +0700

    comit

--- freemode.php
+++ freemode.php
@@ -28,13 +28,16 @@
                             <td class="col-first col-4">9:11</td>
                         </tr>
                         <?php } ?>
+                        <tr class="h30 break">
+                            <td colspan="4" class="text-center">3-minute break</td>
+                        </tr>
                     </table>
                 </div>
 
                 <div class="col-50">
                     <table class="list-player list-bracket">
                         <tr class="h60 label-col">
-                            <td class="col-1 text-left" colspan="3">UB 115 4T — PISTON 54MM FREE  — Group c</td>
+                            <td class="col-1 text-left name-col-1" colspan="3">UB 115 4T — PISTON 54MM FREE  — Group c</td>
                             <td class="text-right"><span class="timemode_gray">TIME MODE</span></td>
                         </tr>
                         <tr class="box-center">
@@ -45,15 +48,15 @@
                                 <ul class="h30 ">
                                     <li class="num1 text-left pl-30">0.71</li>
                                     <li class="num2">RT</li>
-                                    <li class="num3 text-right pr-30"><span>0.68</span></li>
+                                    <li class="num3 text-right pr-30 yellow">0.68</li>
                                 </ul>
                                 <ul class="h30 ">
-                                    <li class="num1 text-left pl-30"><span>8.55</span></li>
+                                    <li class="num1 text-left pl-30 yellow">8.55</li>
                                     <li class="num2">ET</li>
                                     <li class="num3 text-right pr-30">9.78</li>
                                 </ul>
                                 <ul class="h30 ">
-                                    <li class="num1 text-left pl-30"><span>326.40</span></li>
+                                    <li class="num1 text-left pl-30 yellow">326.40</li>
                                     <li class="num2">MPH</li>
                                     <li class="num3 text-right pr-30">450.28</li>
                                 </ul>
@@ -67,14 +70,48 @@
                     <table class="list-player">
                         <tr class="h120 person box-head">
                             <td class="title1 f48 text-left">AT9</td>
-                            <td class="title2 f60 text-left">
+                            <td class="title2 f60 text-left yellow">
                                 <img src="assets/images/img/cup.png" alt="">
-                                <span>8.555</span>
+                                8.555
                             </td>
                             <td class="title3 f30 text-center">TT</td>
                             <td class="title4 f60 text-right">9.789</td>
                             <td class="title5 f48 text-right">VW7</td>
                         </tr>
+                    </table>
+
+                    <table class="list-player other-result">
+                        <tr class="h60 label-col">
+                            <td class="timemode_gray text-right" colspan="9">OTHER RESULT</td>
+                        </tr>
+                        <?php  for($i =0; $i<4; $i++){?>
+                            <tr class="h80 person">
+                                <td class="cot1" rowspan="3"><img src="assets/images/img/tuan.jpg" alt=""></td>
+                                <td class="cot2 f30" rowspan="2"><img src="assets/images/img/cup.png" alt="">AT9</td>
+                                <td class="cot3 f30 text-center yellow" rowspan="2">8.55</td>
+                                <td class="cot4">0.71</td>
+                                <td class="cot5 text-center">RT</td>
+                                <td class="cot6">0.71</td>
+                                <td class="cot7 f30 text-center" rowspan="2">9.78</td>
+                                <td class="cot8 f30" rowspan="2">VW7</td>
+                                <td class="cot9" rowspan="3"><img src="assets/images/img/quang.jpg" alt=""></td>
+                            </tr>
+                            <tr class="h80 person">
+                                <td class="cot4">9.78</td>
+                                <td class="cot5 text-center">ET</td>
+                                <td class="cot6">9.78</td>
+                            </tr>
+                            <tr class="h80 person">
+                                <td class="cot2 f12" colspan="2" >Nguyễn Anh Tuấn</td>
+                                <td class="cot4">326.40</td>
+                                <td class="cot5 text-center">MPH</td>
+                                <td class="cot6">326.40</td>
+                                <td class="cot8 f12" colspan="2">Trần Văn Quang</td>
+                            </tr>
+                            <tr>
+                                <td class="mb-20" colspan="9"></td>
+                            </tr>
+                        <?php }?>
                     </table>
                 </div>
 
--- timemode(brk).php
+++ timemode(brk).php
@@ -10,93 +10,28 @@
                         <td class="col-3">START</td>
                         <td class="col-first col-4">FINISH</td>
                     </tr>
-                    <tr class="h60 person_heading person_active">
-                        <td class="col-1">UB 115 4T
-                            Piston 54mm free</td>
-                        <td class="col-2">1</td>
-                        <td class="col-3">9:00</td>
-                        <td class="col-first col-4">9:10</td>
-                    </tr>
-                    <tr class="h30 break">
-                        <td colspan="4" class="text-center">3-minute break</td>
-                    </tr>
                     <tr class="h60 person">
                         <td class="col-1">UB 115 4T
                             Piston 54mm free</td>
-                        <td class="col-2">2</td>
-                        <td class="col-3">9:13</td>
-                        <td class="col-first col-4">9:23</td>
+                        <td class="col-2">C</td>
+                        <td class="col-3">9:00</td>
+                        <td class="col-first col-4">9:10</td>
                     </tr>
                     <tr class="h30 break active">
                         <td colspan="4" class="text-center">3-minute break</td>
                     </tr>
-                    <tr class="h60 person">
-                        <td class="col-1">UB 115 4T
-                            Piston 54mm free</td>
-                        <td class="col-2">3</td>
-                        <td class="col-3">9:26</td>
-                        <td class="col-first col-4">9:36</td>
-                    </tr>
-                    <tr class="h30 break">
-                        <td colspan="4" class="text-center break_heading">3-minute break</td>
-                    </tr>
-                    <tr class="h60 person">
-                        <td class="col-1">125 2T 62 Piston Free</td>
-                        <td class="col-2">1</td>
-                        <td class="col-3">9:39</td>
-                        <td class="col-first col-4">9:49</td>
-                    </tr>
-                    <tr class="h30 break">
-                        <td colspan="4" class="text-center">3-minute break</td>
-                    </tr>
-                    <tr class="h60 person">
-                        <td class="col-1">125 2T 62 Piston Free</td>
-                        <td class="col-2">2</td>
-                        <td class="col-3">9:52</td>
-                        <td class="col-first col-4">10:02</td>
-                    </tr>
-                    <tr class="h30 break">
-                        <td colspan="4" class="text-center">3-minute break</td>
-                    </tr>
-                    <tr class="h60 person">
-                        <td class="col-1">125 2T 62 Piston Free</td>
-                        <td class="col-2">3</td>
-                        <td class="col-3">10:05</td>
-                        <td class="col-first col-4">10:15</td>
-                    </tr>
-                    <tr class="h30 break">
-                        <td colspan="4" class="text-center">3-minute break</td>
-                    </tr>
-                    <tr class="h60 person">
-                        <td class="col-1">Tay ga nho 4T
-                            Free</td>
-                        <td class="col-2">1</td>
-                        <td class="col-3">10:18</td>
-                        <td class="col-first col-4">10:28</td>
-                    </tr>
-                    <tr class="h30 break">
-                        <td colspan="4" class="text-center">3-minute break</td>
-                    </tr>
-                    <tr class="h60 person">
-                        <td class="col-1">Tay ga nho 4T
-                            Free</td>
-                        <td class="col-2">2</td>
-                        <td class="col-3">10:31</td>
-                        <td class="col-first col-4">10:41</td>
-                    </tr>
-                    <tr class="h30 break">
-                        <td colspan="4" class="text-center">3-minute break</td>
-                    </tr>
-                    <tr class="h60 person">
-                        <td class="col-1">Tay ga nho 4T
-                            Free</td>
-                        <td class="col-2">3</td>
-                        <td class="col-3">10:44</td>
-                        <td class="col-first col-4">10:54</td>
-                    </tr>
-                    <tr class="h30 break">
-                        <td colspan="4" class="text-center">3-minute break</td>
-                    </tr>
+                    <?php for($i=0 ; $i<8; $i++){?>
+                        <tr class="h60 person">
+                            <td class="col-1">UB 115 4T
+                                Piston 54mm free</td>
+                            <td class="col-2">C</td>
+                            <td class="col-3">9:00</td>
+                            <td class="col-first col-4">9:10</td>
+                        </tr>
+                        <tr class="h30 break">
+                            <td colspan="4" class="text-center">3-minute break</td>
+                        </tr>
+                    <?php }?>
                 </table>
             </div>
 
